dc3a65344dba648e8c65c09c1e2cdb02c69c40a3
Greg Orvik <gvorvik@gmail.com>
2018-05-03 10:07:32 -0500

    routes and modules work

---
+++ server/routes/shoes.router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const Shoes = require('../modules/shoe.module');
+
+router.get('/', (req, res) => {
+    res.send(Shoes);
+});
+
+router.post('/', (req, res) => {
+    const newShoes = req.body;
+    console.log(newShoes);    
+    Shoes.push(newShoes);
+    res.send(Shoes);
+});
+
+module.exports = router;
