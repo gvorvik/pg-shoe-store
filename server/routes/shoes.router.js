a4e915cc7bc6839da878b2fa51e42400d8a7a23a
Greg Orvik <gvorvik@gmail.com>
2018-05-03 11:12:04 -0500

    post to database using pg

--- server/routes/shoes.router.js
+++ server/routes/shoes.router.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 const Shoes = require('../modules/shoe.module');
 
-router.get('/', (req, res) => {
-    res.send(Shoes);
-});
+// router.get('/', (req, res) => {
+//     res.send(Shoes);
+// });
 
-router.post('/', (req, res) => {
-    const newShoes = req.body;
-    console.log(newShoes);    
-    Shoes.push(newShoes);
-    res.send(Shoes);
-});
+// router.post('/', (req, res) => {
+//     const newShoes = req.body;
+//     console.log(newShoes);    
+//     Shoes.push(newShoes);
+//     res.send(Shoes);
+// });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+//pg is to SQL as mongoose is to MongoDB
+const pg = require('pg');
+
+const Shoes = require('./modules/shoe.module');
+
+//Capital P = class or constructor!!!
+//This code takes care of connecting to our database
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'shoe_store', //name of database
+    host: 'localhost', // where is database
+    port: 5432, //location of port (5432 is default for postgres)
+    max: 10, //how many connections at one time
+    idleTimeoutMillis: 30000 //30 seconds to try to connect, otherwise cancel query
+});
+
+//these lines aren't needed, but are helpful to know if we connected
+pool.on('connect', () => {
+    console.log('postgresql connected');
+})
+
+pool.on('error', (error) => {
+    console.log('Error with postgresql', error);
+});
 
 const shoeRoute = require('./routes/shoes.router');
 
@@ -9,7 +34,27 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use('/shoes', shoeRoute);
+app.get('/shoes', (req, res) => {
+    res.send(Shoes);
+});
+
+app.post('/shoes', (req, res) => {
+    const shoe = req.body;
+    //the $1, $2, and array after is a way for pg to sanitize information
+    //and prevent malicious users from entering hard coded code to drop table or return
+    //private information
+    pool.query(`INSERT INTO "shoes" ("type", "cost")
+                VALUES ($1, $2);`, [shoe.type, shoe.cost])
+        .then((results) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+});
+
+// app.use('/shoes', shoeRoute);
 
 
 app.listen(PORT, () => {
