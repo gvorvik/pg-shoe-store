a80592e5aa5ee69f6cbc6061c9fbf273bf1a337f
Greg Orvik <gvorvik@gmail.com>
2018-05-03 11:37:54 -0500

    display array of shoes on get request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,4 +5,24 @@ var app = angular.module('ShoeApp', []);
 app.controller('ShoeController', ['$http', function($http) {
     console.log('Shoe Controller Loaded');
     
+    var self = this;
+
+    self.shoeList = [];
+
+    self.getShoes = function() {
+        $http({
+            method: 'GET',
+            url: '/shoes'
+        })
+        .then(function(response) {
+            console.log(response);
+            self.shoeList = response.data;
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    }
+
+    self.getShoes();
+
 }]);
